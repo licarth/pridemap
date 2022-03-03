ad9a021f418c40321a317ff81bdee53ec7952417
Thomas Carli <thomascarli@gmail.com>
2022-03-03 22:46:51 +0100

    do not deactivate shortcuts

--- src/App.js
+++ src/App.js
@@ -10,14 +10,11 @@ function App() {
   const { nextWeekend, previousWeekend } = usePrideSelect();
   useEffect(() => {
     function handleKeyDown(e) {
-      e.preventDefault();
-      console.log(e.keyCode);
       if (e.keyCode === 37) {
         previousWeekend();
       } else if (e.keyCode === 39) {
         nextWeekend();
       }
-      e.stopPropagation();
     }
 
     document.addEventListener("keydown", handleKeyDown);
