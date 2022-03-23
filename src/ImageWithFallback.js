dc21067c16e132f474eedc8764ef4b4307671953
Thomas Carli <thomascarli@gmail.com>
2022-03-23 21:31:11 +0100

    new image mechanism

--- src/ImageWithFallback.js
+++ src/ImageWithFallback.js
@@ -1,11 +1,50 @@
-import ReactImageFallback from "react-image-fallback";
+import { useEffect, useState } from "react";
+import _ from "lodash";
 
-export const ImageWithFallback = ({ pride: { instagram }, className }) => (
-  <ReactImageFallback
-    src={`https://storage.googleapis.com/pridemap-eu-assets/badges/instagram/${instagram}.png`}
-    // fallbackImage="my-backup.png"
-    // initialImage="loader.gif"
-    // alt="cool image should be here"
-    className={className}
-  />
-);
+export const ImageWithFallback = ({
+  pride: { instagram, autoPictureLink, manualPictureLink, twitter, city },
+  className,
+}) => {
+  const [src, setSrc] = useState();
+
+  useEffect(async () => {
+    setSrc(null);
+
+    const links = _.filter(
+      [
+        instagram &&
+          `https://storage.googleapis.com/pridemap-eu-assets/badges/instagram/${instagram}.png`,
+        manualPictureLink,
+        autoPictureLink,
+      ],
+      (i) => !_.isEmpty(i)
+    );
+
+    for (const link of links) {
+      if (await setImageFromLink(link, setSrc)) {
+        break;
+      }
+    }
+  }, [city]);
+
+  return src ? (
+    <img key={`logo-${city}`} src={src} className={className} />
+  ) : (
+    <></>
+  );
+};
+
+async function setImageFromLink(linnk, setSrc) {
+  try {
+    const res = await fetch(linnk);
+    const imageBlob = await res.blob();
+    if (!`${res.status}`.startsWith(2)) {
+      return false;
+    }
+    const imageObjectURL = window.URL.createObjectURL(imageBlob);
+    setSrc(imageObjectURL);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
