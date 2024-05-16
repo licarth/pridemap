6054ff75070f7b75a103672b4f160ae0d4ad04d3
Thomas Carli <thomascarli@gmail.com>
2024-05-16 14:50:50 +0200

    fix parseDate

--- src/parseDate.js
+++ src/parseDate.js
@@ -2,7 +2,7 @@ import { isValid, parse } from "date-fns";
 
 export const parseDate = (string, defaultValue) => {
   try {
-    const newLocal = parse(string, "dd/MM", new Date(2022, 1));
+    const newLocal = parse(string, "dd/MM", new Date(2024, 1));
     return isValid(newLocal) ? newLocal : defaultValue;
   } catch {
     return defaultValue;
